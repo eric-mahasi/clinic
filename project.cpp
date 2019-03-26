01ec468157dafc07c87bfdf47eac8dd5a75d21d1
eric <mahasieric@gmail.com>
2019-03-26 22:41:30 +0300

    Initial commit

---
+++ project.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class clinic{
+
+private:
+  int doctor_choice, reception_choice;
+	string first_name, last_name, gender, age, id_number, insurance, status, app_time;
+
+public:
+  int choice;
+  void main_menu();
+  void doctor_menu();
+  void reception_menu();
+  void input();
+  void display();
+  void get_report();
+  void output();
+  void view_apps();
+};
+
+void clinic::main_menu(){
+  cout << "Please make a choice from the options provided below.\n";
+  cout << "1-----Doctor\n"
+       << "2-----Receptionist\n"
+       << "3-----Quit\n";
+  cin >> choice;
+}
+
+void clinic:: doctor_menu(){
+  cout << "1-----View today's appointments\n"
+       << "2-----Go back to main menu\n"
+       << "3-----Quit\n";
+
+  cin >> doctor_choice;
+  switch(doctor_choice){
+    case 1:
+      clinic::view_apps();
+      break;
+    case 2:
+      clinic::main_menu();
+      break;
+    case 3:
+      exit(0);
+      break;
+  }
+}
+
+void clinic::reception_menu() {
+  cout << "1-----Edit patient details\n"
+       << "2-----Enter new patient details\n"
+       << "3-----Generate report\n"
+       << "4-----View all records\n"
+       << "5-----Go back to main menu\n"
+       << "6-----Quit\n";
+
+  cin >> reception_choice;
+  switch(reception_choice) {
+    case 1:
+      // edit_file();
+      break;
+    case 2:
+      clinic::input();
+      break;
+    case 3:
+      clinic::get_report();
+      break;
+    case 4:
+      clinic::display();
+      break;
+    case 5:
+      clinic::main_menu();
+      break;
+    case 6:
+      exit(0);
+      break;
+
+  }
+}
+
+void clinic::input(){
+  // Takes input from the keyboard and stores it to the file
+  // TODO: Consider renaming to write_to_file
+  int number;
+  ofstream write("hospital records.xls", ios::app);
+  cout << "How many records would you like to add? ";
+  cin >> number;
+
+ for(int n = 0;n < number;n ++){
+
+  cout << "Enter First Name:";
+	cin >> first_name;
+
+	cout << "Enter Last Name:";
+	cin >> last_name;
+
+	cout << "Enter Gender In Full(Male Or Female):";
+	cin >> gender;
+
+	cout << "Enter Age:";
+	cin >> age;
+
+	cout << "Enter ID Number:";
+	cin >> id_number;
+
+	cout << "Enter Insurance Company:";
+	cin >> insurance;
+
+  cout << "Enter Status:";
+  cin >> status;
+
+  system("cls");
+  write << first_name << "\t" << last_name << "\t" << gender << "\t" << age << "\t" << id_number << "\t" << insurance << "\t" << status << endl;
+  }
+  write.close();
+}
+
+void clinic::display(){
+  // Displays all the user records stored in the file
+	ifstream view("hospital records.xls");
+
+  while(view >> first_name >> last_name >> gender >> age >> id_number >> insurance){
+    cout << first_name << "\t" << last_name << "\t" << gender << "\t" << age << "\t" << id_number << "\t" << insurance;
+    cout << endl;
+  }
+  view.close();
+}
+
+void clinic::get_report(){
+  // Determines when a patient's appointment is due
+  ifstream read("hospital records.xls");
+  ofstream output("appointments.xls", ios::app);
+
+  while(read >> first_name >> last_name >> gender >> age >> id_number >> insurance >> status){
+    if(status == "routine"){
+      app_time = "Today";
+      output << first_name << "\t" << last_name << "\t" << gender << "\t" << age << "\t" << id_number << "\t" << insurance << "\t" << app_time << endl;
+    }else{
+      app_time = "Tomorrow";
+    }
+  }
+  output.close();
+  read.close();
+}
+
+void clinic::view_apps(){
+  ifstream read("appointments.xls");
+  while (read >> first_name >> last_name >> gender >> age >> id_number >> insurance >> app_time){
+    cout << first_name << "\t" << last_name << "\t" << gender << "\t" << age << "\t" << id_number << "\t" << insurance;
+  }
+  read.close();
+}
+
+int main(){
+  clinic uzima;
+  cout << "Welcome.\n\n";
+
+  while(true){
+    uzima.main_menu();
+    // system("cls");
+    switch(uzima.choice){
+      case 1:
+        uzima.doctor_menu();
+        break;
+      case 2:
+        uzima.reception_menu();
+        break;
+      case 3:
+        exit(0);
+        break;
+      default:
+        cout << "Invalid choice. Try again \n";
+        break;
+    }
+  }
+  return 0;
+}
